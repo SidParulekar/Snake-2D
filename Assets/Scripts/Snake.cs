2727b7a7b6c1372527b7ebf389a5ad7e79ef1189
SidParulekar <76064146+SidParulekar@users.noreply.github.com>
2025-01-18 16:48:28 +0530

    Implemented Snake Growth Capability on colliding with Food
    Created game object for snake segments and made it a prefab.
    
    Defined list of Transforms to store multiple snake segments.
    
    Implemented the function for snake growth which instantiates the snake segment prefab and adds it to the list of segments every time the snake collides with the food.

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -6,6 +6,16 @@ public class Snake : MonoBehaviour
 {
     private Vector2 direction = Vector2.right;
 
+    private List<Transform> segments;
+
+    [SerializeField]private Transform segmentPreFab;
+
+    private void Start()
+    {
+        segments = new List<Transform>();
+        segments.Add(this.transform);
+    }
+
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.W))
@@ -31,8 +41,31 @@ public class Snake : MonoBehaviour
 
     private void FixedUpdate() 
     {
+
+        for(int i = segments.Count-1; i>0; i--)
+        {
+            segments[i].position = segments[i - 1].position;
+        }
+
         this.transform.position = new Vector3(Mathf.Round(this.transform.position.x + direction.x),
                                               Mathf.Round(this.transform.position.y + direction.y),
                                               0.0f);
+    }
+
+    private void Grow()
+    {
+        Transform segment = Instantiate(this.segmentPreFab);
+
+        segment.position = segments[segments.Count - 1].position;
+
+        segments.Add(segment);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.GetComponent<Food>())
+        {
+            Grow();
+        }
     }
 }
